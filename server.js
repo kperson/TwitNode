d2ddf7e36edc2fe3c82673609f77869191f08e3f
kperson <kelton.person@gmail.com>
2011-07-29 09:59:55 -0300

    Edited server.js via GitHub

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var validationDAO = require('./dao/validation.js');
 var handler = {
 	
 	searchTweets: function (term, memberId, companyName, ticker, since, slug, industry, handle, industrySlug, sector, sectorSlug, showSlugs, order, orderDirection, start, limit, score, login, success){
-		validationDAO.validateLogin('searchTweets', login.username, login.password, function(client){
+		console.log(server.connections);
+validationDAO.validateLogin('searchTweets', login.username, login.password, function(client){
 			tweetDAO.search(term, memberId, companyName, ticker, since, slug, industry, handle, industrySlug, sector, sectorSlug, showSlugs, order, orderDirection, start, limit, score, 
 				function(resultRs){
 					success(resultRs);
